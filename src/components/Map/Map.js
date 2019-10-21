1bfa99114ed30f87d658658226ced0eb68b4f525
Edith Emmings <emmings.edith@gmail.com>
2019-10-21 12:40:38 -0500

    google maps is working on the details page

---
+++ src/components/Map/Map.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=place&key=AIzaSyCsBnZcbDFpKB4Y5uYIN6Xvsr9N8U2hq7s`,
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+)((props) =>
+
+    <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{ lat: 44.952228, lng: -93.2791737 }}
+    >
+        {props.coords ?
+        // {true  ?
+            <Marker 
+                position={{ lat: props.coords.location.lat, lng: props.coords.location.lng }}
+                // position={{ lat: 44.952228, lng: -93.2791737}}
+                label={props.name}
+                labelAnchor={new window.google.maps.Point(0, 0)}
+                labelStyle={{ backgroundColor: "yellow", fontSize: "14px", padding: "16px" }}
+            >
+            </Marker > 
+        : ''}
+    </GoogleMap>
+)
+
+
+export default MyMapComponent;
--- src/components/ShelterListingPage/ShelterDetails/ShelterDetails.js
+++ src/components/ShelterListingPage/ShelterDetails/ShelterDetails.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import Map from '../../Map/Map'
+import axios from 'axios'
 
 class ShelterDetails extends Component {
+    state = { googlePlace: {} }
+    
+    componentDidMount() {
+        let location = '';
+        this.props.reduxState.shelters.forEach((shelter) => {
+            if (shelter.id == this.props.match.params.id) {
+                location = shelter.location;
+            }
+        })
+        this.getSelection(location)
+    }
+
+    getSelection = (location) => {
+        let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyCsBnZcbDFpKB4Y5uYIN6Xvsr9N8U2hq7s&fields=photos,formatted_address,name,rating,opening_hours,geometry&inputtype=textquery&input=${location}`
+        let proxy = "https://cors-anywhere.herokuapp.com/"
+        axios({
+            method: 'GET',
+            url: proxy + url,
+            proxyurl: proxy
+        })
+            .then((response) => {
+                console.log(location, response.data)
+                this.setState({
+                    ...this.state,
+                    googlePlace: response.data.candidates[0]
+                })
+            }).catch(error => {
+                console.log('error finding place: ', error)
+            })
+    }
     render() {
         return (
             <>
                 {Array.isArray(this.props.reduxState.shelters) ?
                     <div className='listingCard' onClick={this.goToDetailsPage}>
                         {this.props.reduxState.shelters.map((shelter) => {
-                            if (shelter.id == this.props.match.params.id){
+                            if (shelter.id == this.props.match.params.id) {
                                 return <div key={shelter.id}>
                                     <h3>{shelter.name}</h3>
                                     <p>{shelter.location}</p>
@@ -24,7 +56,12 @@ class ShelterDetails extends Component {
                                             return <li>{type.type}, {type.count}/{type.capacity}</li>
                                         })}
                                     </ul>
-                                    {/* <li>{JSON.stringify(shelter)}</li> */}
+                                    {this.state.googlePlace ?
+                                        <Map
+                                            coords={this.state.googlePlace.geometry}
+                                            name={shelter.name}
+                                        />
+                                        : ''}
                                 </div>
                             }
                         })}</div>
