15ea8c2213f7237b085b310fe9cecf700192c8fa
Edith Emmings <emmings.edith@gmail.com>
2019-10-24 14:44:22 -0500

    conditional render on home page

--- src/components/UserPortal/UserPortal.js
+++ src/components/UserPortal/UserPortal.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import UserPortalHome from './UserPortalHome/UserPortalHome'
-import SignUp1Contact from './SignUp/SignUp1Contact/SignUp1Contact'
-
+import { Button } from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
 
 class ShelterPortalHome extends Component {
     componentDidMount() {
@@ -13,7 +13,11 @@ class ShelterPortalHome extends Component {
             <div>
                 {this.props.reduxState.userShelter.types
                     ? <UserPortalHome shelter={this.props.reduxState.userShelter} />
-                    : <h1>LOADING</h1>
+                    : <div>
+                        <p>Waiting for your info ...</p>
+                        <p>Still need to register your shelter?</p>
+                        <Link to='/sign-up-1'><Button>Click here!</Button></Link>
+                    </div>
                 }
                 {/* {!this.props.reduxState.userShelter.types 
                     ? <div><SignUp1Contact toPage2={this.toPage2} />
