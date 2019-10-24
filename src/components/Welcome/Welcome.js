0b8b0735ff1e53495937fd544afe9a251e83d95d
Edith Emmings <emmings.edith@gmail.com>
2019-10-24 17:42:45 -0500

    styling hero

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import './Welcome.css'
 
 class Welcome extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to Open Doors</h1>
-                <Link to='/explore'><Button primary>Find a Shelter</Button>
-                </Link>
-                <Link to='/home'><Button secondary>Shelter Portal</Button>
-                </Link>
-
+            <div className='Welcome'>
+                <div className='purple'>
+                    <h1><span className='welcomeTo'>Welcome to</span>Open Doors</h1>
+                    <div className='exploreBtn'>
+                        <Link to='/explore'><Button size='massive'>Find a Shelter</Button>
+                        </Link>
+                    </div>
+                    <div className='homeBtn'>
+                        <Link to='/home'><Button inverted compact>Shelter Portal</Button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         )
     }
