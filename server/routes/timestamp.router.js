3a94521cf10c0d3b519c17dc10ebba224846cf23
Edith Emmings <emmings.edith@gmail.com>
2019-11-02 12:21:47 -0500

    column name change

--- server/routes/timestamp.router.js
+++ server/routes/timestamp.router.js
@@ -7,14 +7,14 @@ const pool = require('../modules/pool');
 // was updated
 
 router.get('/:id', (req, res) => {
-    let queryText = `SELECT "lastupdate" FROM "shelter" WHERE "id" = $1;`;
+    let queryText = `SELECT "timestamp" FROM "shelter" WHERE "id" = $1;`;
     pool.query(queryText, [ Number(req.params.id) ])
         .then((response) => res.send(response.rows))
         .catch((error) => { res.sendStatus(500); console.log(error) });
 });
 
 router.put('/', (req, res) => {
-    const queryText = `UPDATE "shelter" SET "lastupdate" = $1 WHERE "user_id" = $2;`;
+    const queryText = `UPDATE "shelter" SET "timestamp" = $1 WHERE "user_id" = $2;`;
     pool.query(queryText, [req.body.timestamp, req.user.id])
         .then(() => res.sendStatus(201))
         .catch((error ) => {res.sendStatus(500); console.log(error)});
